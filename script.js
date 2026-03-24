1c69e1feb54d45183f25a3046ce69f6ba6a12272
pronote-ent <pronote-ent@proton.me>
2026-03-24 17:56:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,18 +6,48 @@ if (form) {
     e.preventDefault();
 
     const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const errorEmail = document.getElementById("errorEmail");
+    const errorPassword = document.getElementById("errorPassword");
+
+    // reset erreurs
+    errorEmail.textContent = "";
+    errorPassword.textContent = "";
+
+    // regex email
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (regex.test(email)) {
+    // identifiants autorisés
+    const allowedEmail = "lehnazer@gmail.com";
+    const allowedPassword = "Lehna93200";
+
+    let valid = true;
+
+    // vérif email
+    if (!regex.test(email)) {
+      errorEmail.textContent = "Format d'email invalide";
+      valid = false;
+    } else if (email !== allowedEmail) {
+      errorEmail.textContent = "Email non reconnu";
+      valid = false;
+    }
+
+    // vérif password
+    if (password !== allowedPassword) {
+      errorPassword.textContent = "Mot de passe incorrect";
+      valid = false;
+    }
+
+    // si tout est OK
+    if (valid) {
       localStorage.setItem("logged", "true");
       window.location.href = "accueil.html";
-    } else {
-      alert("Email invalide");
     }
   });
 }
 
-// PROTECTION
+// PROTECTION DES PAGES
 if (!window.location.pathname.includes("index.html")) {
   const isLogged = localStorage.getItem("logged");
 
